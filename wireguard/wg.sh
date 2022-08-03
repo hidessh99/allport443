7415bb0f149b6abe036bbefe020b5a20759cf798
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-04 04:30:54 +0700

    Update wg.sh

--- wireguard/wg.sh
+++ wireguard/wg.sh
@@ -19,7 +19,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 # ==================================================
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/samratu/large/sae/wireguard"
+wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/large/sae/wireguard"
 
 # Check OS version
 if [[ -e /etc/debian_version ]]; then
