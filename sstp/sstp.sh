7a31ed8ee14f9f0a6d3abcf3a7faa7556f4de575
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-04 04:29:44 +0700

    Update sstp.sh

--- sstp/sstp.sh
+++ sstp/sstp.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/samratu/large/sae/sstp"
+wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/large/sae/sstp"
 
 MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
