09f726e36d77afa1c256b934709835fb1dbd05f3
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-04 04:27:30 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/samratu/large/sae/backup"
+wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/large/sae/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
