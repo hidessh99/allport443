b071d7274a530c3c0bcb64588b85474a0cf7f60a
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-04 04:10:22 +0700

    Add files via upload

---
+++ ipsec/addl2tp.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# My Telegram : https://t.me/zerossl
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+if [[ "$IP" = "" ]]; then
+PUBLIC_IP=$(wget -qO- ipinfo.io/ip);
+else
+PUBLIC_IP=$IP
+fi
+source /var/lib/wisnucs/ipvps.conf
+if [[ "$IP2" = "" ]]; then
+#domain=$(cat /etc/v2ray/domain)
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP2
+fi
+until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Username : " -e VPN_USER
+		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/wisnucs/data-user-l2tp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo -e "Username ${RED}${VPN_USER}${NC} Already On VPS Please Choose Another"
+			exit 1
+		fi
+	done
+read -p "Password : " VPN_PASSWORD
+read -p "Expired (Days) : " masaaktif
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+clear
+
+# Add or update VPN user
+cat >> /etc/ppp/chap-secrets <<EOF
+"$VPN_USER" l2tpd "$VPN_PASSWORD" *
+EOF
+
+VPN_PASSWORD_ENC=$(openssl passwd -1 "$VPN_PASSWORD")
+cat >> /etc/ipsec.d/passwd <<EOF
+$VPN_USER:$VPN_PASSWORD_ENC:xauth-psk
+EOF
+
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
+echo -e "### $VPN_USER $exp">>"/var/lib/wisnucs/data-user-l2tp"
+cat <<EOF
+
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[46;1;46m        🔰 AKUN L2TP 🔰           \E[0m"   
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "IP/Host    : $PUBLIC_IP"
+echo -e "Domain     : $domain"
+echo -e "IPsec PSK  : gandring"
+echo -e "Username   : $VPN_USER"
+echo -e "Password   : $VPN_PASSWORD"
+echo -e "Created    : $hariini"
+echo -e "Expired    : $exp"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[46;1;46m  🔰LUXURY EDITION BY ZEROSSL🔰   \E[0m"   
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
---
+++ ipsec/addpptp.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# My Telegram : https://t.me/zerossl
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+if [[ "$IP" = "" ]]; then
+PUBLIC_IP=$(wget -qO- ipinfo.io/ip);
+else
+PUBLIC_IP=$IP
+fi
+source /var/lib/wisnucs/ipvps.conf
+if [[ "$IP2" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+#domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP2
+fi
+until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Username : " -e VPN_USER
+		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/wisnucs/data-user-pptp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo -e "Username ${RED}${VPN_USER}${NC} Already On VPS Please Choose Another"
+			exit 1
+		fi
+	done
+read -p "Password : " VPN_PASSWORD
+read -p "Expired (Days) : " masaaktif
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+clear
+
+# Add or update VPN user
+cat >> /etc/ppp/chap-secrets <<EOF
+"$VPN_USER" pptpd "$VPN_PASSWORD" *
+EOF
+
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets*
+echo -e "### $VPN_USER $exp">>"/var/lib/wisnucs/data-user-pptp"
+cat <<EOF
+
+============================
+PPTP VPN
+============================
+IP/Host   : $PUBLIC_IP
+Domain    : $domain
+Username  : $VPN_USER
+Password  : $VPN_PASSWORD
+Created   : $hariini
+Expired   : $exp
+============================
+Script By @zerossl
+EOF
