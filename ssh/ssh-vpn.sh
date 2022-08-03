8b548db764d8365b711c5f443835f9445b380044
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-04 04:32:08 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -15,18 +15,18 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 #MYIP=$(wget -qO- https://ipv4.icanhazip.com);
 MYIP6=$(wget -qO- https://ipv6.icanhazip.com);
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/samratu/large/sae/ssh"
+wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/large/sae/ssh"
 
 # Link Hosting Kalian Untuk Xray
-wisnuvpnn="raw.githubusercontent.com/samratu/large/sae/xray"
+wisnuvpnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/xray"
 
 # Link Hosting Kalian Untuk Trojan Go
-wisnuvpnnn="raw.githubusercontent.com/samratu/large/sae/trojango"
+wisnuvpnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/trojango"
 
 # Link Hosting Kalian Untuk Stunnel5
-wisnuvpnnnn="raw.githubusercontent.com/samratu/large/sae/stunnel5"
+wisnuvpnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/stunnel5"
 
-wisnuvpnnnnn="raw.githubusercontent.com/samratu/large/sae/update"
+wisnuvpnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/update"
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
 MYIP=$(wget -qO- ipinfo.io/ip);
