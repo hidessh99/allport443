8d607aa9f5cbeda50600917ca18ef13763ae566a
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-05 11:01:41 +0700

    Update renewxrayss.sh

--- xray/renewxrayss.sh
+++ xray/renewxrayss.sh
@@ -35,6 +35,69 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/xvless.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Select the existing client you want to renew"
+	echo " Press CTRL+C to return"
+	echo -e "==============================="
+	grep -E "^### " "/etc/xray/xvless.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/xvmess.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Select the existing client you want to renew"
+	echo " Press CTRL+C to return"
+	echo -e "==============================="
+	grep -E "^### " "/etc/xray/xvmess.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/xss.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Select the existing client you want to renew"
+	echo " Press CTRL+C to return"
+	echo -e "==============================="
+	grep -E "^### " "/etc/xray/xss.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
 read -p "Expired (Days): " masaaktif
 user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
@@ -46,6 +109,19 @@ exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
 sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json
 sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xss.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xss.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xvmess.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xvmess.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xvless.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xvless.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xtrojan.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xtrojan.json
+systemctl restart xss
+systemctl restart config
+systemctl restart xvless
+systemctl restart xvmess
+systemctl restart xtrojan
 systemctl restart xray.service
 service cron restart
 clear
