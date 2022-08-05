4df640fd3fd2ab53c5c3901e36dc0329b1955151
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-05 07:56:46 +0700

    Update addtrojangrpc.sh

--- xray/addtrojangrpc.sh
+++ xray/addtrojangrpc.sh
@@ -78,6 +78,7 @@ systemctl restart xray.service
 systemctl restart xtrojan.service
 systemctl restart xvless
 systemctl restart xvmess
+systemctl restart xss
 service cron restart
 clear
 echo -e ""
