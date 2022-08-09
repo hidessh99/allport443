f0d3a4f0971288d0b053f8f2d8a2ff0efbe78f0b
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-09 21:06:01 +0700

    Update python.sh

--- websocket/python.sh
+++ websocket/python.sh
@@ -59,6 +59,33 @@ systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
 # Getting Proxy Template
+wget -q -O /usr/local/bin/ovpn-tls https://${wisnuvpn}/ovpn-tls.py
+chmod +x /usr/local/bin/ovpn-tls
+
+# Installing Service
+cat > /etc/systemd/system/ovpn-tls.service << END
+[Unit]
+Description=OVPN WEBSOCKET ROTING PENGKOL BY GANDRING
+Documentation=https://t.me/zerossl
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ovpn-tls 2083
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ovpn-tls
+systemctl restart ovpn-tls
+
+# Getting Proxy Template
 wget -q -O /usr/local/bin/ws-tls https://${wisnuvpn}/ws-tls.py
 chmod +x /usr/local/bin/ws-tls
 
