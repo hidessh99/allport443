1988d1b8e2d12b51260c8314a86f0d9c09495d7b
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-12 11:03:54 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -366,8 +366,8 @@ chmod 644 /etc/stunnel5
 
 # Download Config Stunnel5
 cat > /etc/stunnel5/stunnel5.conf <<-END
-cert = /etc/xray/xray.crt
-key = /etc/xray/xray.key
+#cert = /etc/xray/xray.crt
+#key = /etc/xray/xray.key
 cert = /etc/stunnel5/stunnel5.pem
 client = no
 socket = a:SO_REUSEADDR=1
