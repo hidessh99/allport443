36de2bc386fbcb87f01c97503bccb6c67d67e9f0
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-13 05:47:55 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -571,62 +571,70 @@ systemctl start xvmess.service
 systemctl restart xvmess.service
 
 # Install Trojan Go
-mkdir -p /usr/lib/trojan-go >/dev/null 2>&1
-wget -q -N --no-check-certificate https://github.com/p4gefau1t/trojan-go/releases/download/$(curl -fsSL https://api.github.com/repos/p4gefau1t/trojan-go/releases | grep '"tag_name":' | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')/trojan-go-linux-amd64.zip 
-unzip -o -d /usr/lib/trojan-go/ ./trojan-go-linux-amd64.zip >/dev/null 2>&1
-mv /usr/lib/trojan-go/trojan-go /usr/local/bin/ >/dev/null 2>&1
+latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/v${latest_version}/trojan-go-linux-amd64.zip"
+mkdir -p "/usr/bin/trojan-go"
+mkdir -p "/etc/trojan-go"
+cd `mktemp -d`
+curl -sL "${trojango_link}" -o trojan-go.zip
+unzip -q trojan-go.zip && rm -rf trojan-go.zip
+mv trojan-go /usr/local/bin/trojan-go
 chmod +x /usr/local/bin/trojan-go
-rm -rf ./trojan-go-linux-amd64.zip >/dev/null 2>&1
+mkdir /var/log/trojan-go/
 touch /etc/trojan-go/akun.conf
 touch /var/log/trojan-go/trojan-go.log
 
-domain=$(cat /root/domain)
 # Buat Config Trojan Go
-cat <<EOF > /etc/trojan-go/config.json
+cat > /etc/trojan-go/config.json << END
 {
   "run_type": "server",
   "local_addr": "0.0.0.0",
   "local_port": 2053,
   "remote_addr": "127.0.0.1",
-  "remote_port": 80,
+  "remote_port": 88,
   "log_level": 1,
-  "log_file": "/var/log/trojan-go.log",
+  "log_file": "/var/log/trojan-go/trojan-go.log",
   "password": [
-        "$uuid"
+      "$uuid"
   ],
   "disable_http_check": true,
   "udp_timeout": 60,
   "ssl": {
     "verify": false,
     "verify_hostname": false,
-    "cert": "/root/.acme.sh/${domain}_ecc/fullchain.cer",
-    "key": "/root/.acme.sh/${domain}_ecc/${domain}.key",
+    "cert": "/etc/ssl/private/fullchain.pem",
+    "key": "/etc/ssl/private/privkey.pem",
     "key_password": "",
     "cipher": "",
     "curves": "",
     "prefer_server_cipher": false,
-    "sni": "",
+    "sni": "$domain",
     "alpn": [
       "http/1.1"
     ],
     "session_ticket": true,
     "reuse_session": true,
     "plain_http_response": "",
-    "fallback_addr": "",
-    "fallback_port": 0,
-    "fingerprint": ""
+    "fallback_addr": "127.0.0.1",
+    "fallback_port": 2053,
+    "fingerprint": "firefox"
   },
   "tcp": {
     "no_delay": true,
     "keep_alive": true,
     "prefer_ipv4": true
   },
+  "mux": {
+    "enabled": false,
+    "concurrency": 8,
+    "idle_timeout": 60
+  },
   "websocket": {
     "enabled": true,
     "path": "/gandring",
-    "host": "${domain}"
+    "host": "$domain"
   },
-  "api": {
+    "api": {
     "enabled": false,
     "api_addr": "",
     "api_port": 0,
@@ -639,38 +647,33 @@ cat <<EOF > /etc/trojan-go/config.json
     }
   }
 }
-EOF
+END
 
-sleep 1
-echo -e "[ ${green}INFO$NC ] Creating service trojan-go"
-cat <<EOF> /etc/systemd/system/trojan-go.service
+# Installing Trojan Go Service
+cat > /etc/systemd/system/trojan-go.service << END
 [Unit]
-Description=Trojan-Go - An unidentifiable mechanism that helps you bypass GFW
-Documentation=https://p4gefau1t.github.io/trojan-go/
+Description=Trojan-Go Service Mod By zerossl
+Documentation=https://t.me/zerossl
 After=network.target nss-lookup.target
 
 [Service]
-Type=simple
-StandardError=journal
+User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart="/usr/local/bin/trojan-go" -config "/etc/trojan-go/config.json"
-LimitNOFILE=51200
+ExecStart=/usr/local/bin/trojan-go -config /etc/trojan-go/config.json
 Restart=on-failure
-RestartSec=1s
+RestartPreventExitStatus=23
 
 [Install]
 WantedBy=multi-user.target
+END
 
-EOF
-chmod +x /etc/trojan-go/config.json
-
-cat <<EOF > /etc/trojan-go/uuid.txt
+# Trojan Go Uuid
+cat > /etc/trojan-go/uuid.txt << END
 $uuid
-EOF
-# // Enable & Start Service
-# Accept port Xray
+END
+
 sudo iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2096 -j ACCEPT
 sudo iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2087 -j ACCEPT
 sudo iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
