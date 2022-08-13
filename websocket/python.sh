8d7fc164164fe9c545171638d83c4e86503fc231
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-13 17:21:16 +0700

    Update python.sh

--- websocket/python.sh
+++ websocket/python.sh
@@ -21,7 +21,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls
 Restart=on-failure
 
 [Install]
