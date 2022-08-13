754a40a8d0c7f5191827cfd0a2130f454282ab58
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-13 22:19:25 +0700

    Update vpn.sh

--- ssh/vpn.sh
+++ ssh/vpn.sh
@@ -51,12 +51,12 @@ systemctl enable --now openvpn-server@server-udp
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
-# Buat config client TCP 700
+# Buat config client TCP 600
 cat > /etc/openvpn/tcp.ovpn <<-END
 client
 dev tun
 proto tcp
-remote xxxxxxxxx 700
+remote xxxxxxxxx 600
 resolv-retry infinite
 route-method exe
 nobind
@@ -109,7 +109,7 @@ cd
 # pada tulisan xxx ganti dengan alamat ip address VPS anda 
 /etc/init.d/openvpn restart
 
-# masukkan certificatenya ke dalam config client TCP 700
+# masukkan certificatenya ke dalam config client TCP 600
 echo '<ca>' >> /etc/openvpn/tcp.ovpn
 cat /etc/openvpn/server/ca.crt >> /etc/openvpn/tcp.ovpn
 echo '</ca>' >> /etc/openvpn/tcp.ovpn
