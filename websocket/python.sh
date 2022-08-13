0fe4ee1099d58b0931981acd6eb26c157b4c88f8
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-14 01:37:01 +0700

    Update python.sh

--- websocket/python.sh
+++ websocket/python.sh
@@ -32,6 +32,33 @@ systemctl daemon-reload
 systemctl enable ws-tls
 systemctl restart ws-tls
 
+# Getting Proxy Template
+wget -q -O /usr/local/bin/wsstunnel https://${wisnuvpn}/wsstunnel.py
+chmod +x /usr/local/bin/ws-tls
+
+# Installing Service
+cat > /etc/systemd/system/wsstunnel.service << END
+[Unit]
+Description=SSH WEBSOCKET TLS ROUTING INDONESIA BY ZEROSSL
+Documentation=https://t.me/zerossl
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/wsstunnel 2087
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable wsstunnel
+systemctl restart wsstunnel
+
 # Installing Service
 cat > /etc/systemd/system/ws-nontls.service << END
 [Unit]
