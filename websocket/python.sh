359ffe7c2380ecfef13c0d1447d89fdcb42fc006
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-13 17:17:23 +0700

    Create python.sh

---
+++ websocket/python.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Proxy mod
+# wisnucokrosatrio
+# ==========================================
+# Link Hosting Kalian
+wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/large/sae/websocket"
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-tls https://${wisnuvpn}/ws-tls.py
+chmod +x /usr/local/bin/ws-tls
+
+# Installing Service
+cat > /etc/systemd/system/ws-tls.service << END
+[Unit]
+Description=SSH WEBSOCKET TLS ROUTING INDONESIA BY ZEROSSL
+Documentation=https://t.me/zerossl
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-tls
+systemctl restart ws-tls
