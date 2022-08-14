438d12e8a0ff323c89599dd25f65be654de297b6
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-14 11:18:32 +0700

    Update sshws-true.sh

--- ssh/sshws-true.sh
+++ ssh/sshws-true.sh
@@ -4,7 +4,12 @@ portdb=`cat /root/log-install.txt | grep -w "PORT DROPBEAR" | cut -d: -f2|sed 's
 portdb2=`cat /root/log-install.txt | grep -w "PORT DROPBEAR" | cut -d: -f2|sed 's/ //g' | cut -f1 -d","`
 portsshws=`cat /root/log-install.txt | grep -w "WEBSOCKET NON TLS" | cut -d: -f2 | awk '{print $1}'`
 portsshwstls=`cat /root/log-install.txt | grep -w "WEBSOCKET TLS" | cut -d: -f2 | awk '{print $1}'`
+portovpnws=`cat /root/log-install.txt | grep -w "OVPN WEBSOCKET NON TLS" | cut -d: -f2 | awk '{print $1}'`
+portovpnwstls=`cat /root/log-install.txt | grep -w "OVPN WEBSOCKET TLS" | cut -d: -f2 | awk '{print $1}'`
+portovpntcp=`cat /root/log-install.txt | grep -w "OVPN TCP" | cut -d: -f2 | awk '{print $1}'`
 
 tmux new-session -d -s sshws "node /usr/bin/proxy3.js -dport $portdb -mport $portsshws -o /root/sshws.log"
+tmux new-session -d -s ovpnws "node /usr/bin/proxy3.js -dport $portovpntcp -mport $portovpnws -o /root/sshws.log"
 tmux new-session -d -s sshwsssl "node /usr/bin/proxy3.js -dport $portdb -mport 600"
+tmux new-session -d -s ovpnwsssl "node /usr/bin/proxy3.js -dport $portovpntcp -mport 100"
 importantfile
