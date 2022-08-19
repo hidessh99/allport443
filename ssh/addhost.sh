2f768ffa63db8c7bab324112d824be7df054be9a
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-19 15:48:57 +0700

    Update addhost.sh

--- ssh/addhost.sh
+++ ssh/addhost.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- https://icanhazip.com);
-DOMAIN=$(wget -qO- https://icanhazip.com);
-MYIP=$domain
-read -p "Masukan Subdomain Anda :" sub
-curl https://get.acme.sh | sh
-alias acme.sh=~/.acme.sh/acme.sh
-/root/.acme.sh/acme.sh --upgrade --auto-upgrade
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-256
-/root/.acme.sh/acme.sh --install-cert -d "${domain}" --ecc \
---fullchain-file /etc/ssl/private/fullchain.pem \
---key-file /etc/ssl/private/privkey.pem
-chown -R nobody:nogroup /etc/xray
-chown -R nobody:nogroup /etc/ssl/private
-chmod 644 /etc/ssl/private/fullchain.pem
-chmod 644 /etc/ssl/private/privkey.pem
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+read -rp "Domain/Host : " -e domain
+echo "IP=$domain" >>/var/lib/wisnucs/ipvps.conf
+rm -rf /etc/xray/domain
+echo $domain > /etc/xray/domain
+certv2ray
 
-cp /etc/ssl/private/fullchain.pem /etc/xray/xray.crt
-cp etc/ssl/private/privkey.pem /etc/xray/xray.key
+
