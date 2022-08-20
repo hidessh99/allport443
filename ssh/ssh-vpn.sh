71917ca06cd5408f3206f84776371519e8d4b801
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-20 10:54:59 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -310,7 +310,7 @@ RUN=yes
 # systemd users: don't forget to modify /lib/systemd/system/sslh.service
 DAEMON=/usr/sbin/sslh
 
-DAEMON_OPTS="--user sslh --listen 0.0.0.0:2087 --ssl 127.0.0.1:500 --ssh 127.0.0.1:300 --ssh 127.0.0.1:2242 --ssh 127.0.0.1:600 --openvpn 127.0.0.1:700 --openvpn 127.0.0.1:100 --http 127.0.0.1:900 --pidfile /var/run/sslh/sslh.pid"
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:2087 --tls 127.0.0.1:8443 --ssh 127.0.0.1:300 --ssh 127.0.0.1:600 --openvpn 127.0.0.1:700 --http 127.0.0.1:900 --pidfile /var/run/sslh/sslh.pid"
 
 END
 
@@ -370,12 +370,8 @@ accept = 400
 connect = 127.0.0.1:300
 
 [ws-tunnel]
-accept = 450
-connect = 127.0.0.1:600
-
-[wsstunnel]
 accept = 2083
-connect = 127.0.0.1:100
+connect = 127.0.0.1:600
 
 [openvpn]
 accept = 900
