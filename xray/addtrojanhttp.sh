75e97d6daff48eee74707a80cd25d03fba333110
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-21 02:48:40 +0700

    Update addtrojanhttp.sh

--- xray/addtrojanhttp.sh
+++ xray/addtrojanhttp.sh
@@ -58,7 +58,8 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 			exit 1
 		fi
 	done
-uuid=$(openssl rand -base64 16)
+#uuid=$(openssl rand -base64 16)
+uuid=$(openssl rand -hex 7)
 read -p "Expired (Days) : " masaaktif
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
