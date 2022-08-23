c2793d5394738e1765fdc045321735381f595961
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-23 10:33:44 +0700

    Update addvmesskcp.sh

--- xray/addvmesskcp.sh
+++ xray/addvmesskcp.sh
@@ -17,7 +17,8 @@ MYIP=$(wget -qO- https://ipv4.icanhazip.com);
 MYIP6=$(wget -qO- https://ipv6.icanhazip.com);
 clear
 domain=$(cat /etc/xray/domain)
-vmkcp="$(cat ~/log-install.txt | grep -w "VMESS KCP" | cut -d: -f2|sed 's/ //g')"
+vmkcp="$(cat ~/log-install.txt | grep -w "VMESS KCP TLS" | cut -d: -f2|sed 's/ //g')"
+vmkcpnon="$(cat ~/log-install.txt | grep -w "VMESS KCP NON TLS" | cut -d: -f2|sed 's/ //g')"
 nontls="$(cat ~/log-install.txt | grep -w "VMESS WS NON TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Username : " -e user
@@ -97,15 +98,32 @@ cat>/etc/xray/vmess-$user-tls.json<<EOF
       "path": "shanumkcp",
       "type": "none",
       "host": "",
+      "tls": "tls"
+}
+EOF
+
+cat>/etc/xray/vmess-$user-nontls.json<<EOF
+      {
+      "v": "4",
+      "ps": "🔰VMESS KCP NON TLS ${user}",
+      "add": "${domain}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "kcp",
+      "path": "shanumkcp",
+      "type": "none",
+      "host": "",
       "tls": "none"
 }
 EOF
+
 vmess_base641=$( base64 -w 0 <<< $vmess_json1)
-#vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
 vmesskcp="vmess://$(base64 -w 0 /etc/xray/vmess-$user-tls.json)"
-#vmess2="vmess://$(base64 -w 0 /etc/xray/vmess-$user-nontls.json)"
+vmesskcpnon="vmess://$(base64 -w 0 /etc/xray/vmess-$user-nontls.json)"
 rm -rf /etc/xray/vmess-$user-tls.json
-#rm -rf /etc/xray/vmess-$user-nontls.json
+rm -rf /etc/xray/vmess-$user-nontls.json
 systemctl restart xray.service
 systemctl restart xvmess
 systemctl restart xvless
@@ -129,7 +147,7 @@ echo -e "Kadaluarsa  :$exp"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "VMESS KCP TLS: ${vmesskcp}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-#echo -e "VMESS WS NONTLS: ${vmess2}"
+echo -e "VMESS KCP NONTLS: ${vmesskcpnon}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m🔰LUXURY EDITION ZEROSSL🔰\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
