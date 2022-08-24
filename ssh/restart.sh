218c46fa928b9c34a606f1b9f0ce767560ed8f0c
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-24 10:38:25 +0700

    Update restart.sh

--- ssh/restart.sh
+++ ssh/restart.sh
@@ -17,9 +17,9 @@ echo -e "Memulai Ulang Layanan Tunnel"
 sleep 2
 systemctl restart ssrmu
 systemctl restart ws-tls
-systemctl restart ovpn-tls
-systemctl restart ovpn-nontls
-systemctl restart stunnel-ohp
+#systemctl restart ovpn-tls
+#systemctl restart ovpn-nontls
+#systemctl restart stunnel-ohp
 systemctl restart xray.service
 systemctl restart shadowsocks-libev
 systemctl restart xl2tpd
