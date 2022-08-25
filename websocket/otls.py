8b757d64d1ea7b1325a2d8b283a398445c0c2cd0
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-26 06:18:32 +0700

    Update otls.py

--- websocket/otls.py
+++ websocket/otls.py
@@ -1,11 +1,10 @@
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
-LISTENING_ADDR = '127.0.0.1'
+LISTENING_ADDR = '0.0.0.0'
 if sys.argv[1:]:
   LISTENING_PORT = sys.argv[1]
-else:
-  LISTENING_PORT = 2083
+  
 #Pass
 PASS = ''
 
