009154f65fc2b39b00f3bf3d364fdfeaeafe1fc4
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-26 08:23:17 +0700

    Update addtrojanhdua.sh

--- xray/addtrojanhdua.sh
+++ xray/addtrojanhdua.sh
@@ -100,3 +100,6 @@ echo -e "TROJAN H2C TLS:  ${trojanhdua}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m🔰LUXURY EDITION ZEROSSL🔰\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
+menu
