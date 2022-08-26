409775c38fb1b1e6e58f52f0cc35f9bc415c8573
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-27 05:26:41 +0700

    Update addvless.sh

--- xray/addvless.sh
+++ xray/addvless.sh
@@ -96,7 +96,7 @@ systemctl restart xvless
 systemctl restart xvmess
 systemctl restart xtrojan
 systemctl restart xss
-systemctl restart nginx
+#systemctl restart nginx
 service cron restart
 clear
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
