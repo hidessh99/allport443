9f2ee13b9c08de27c68e4fb655aca73fe3b90b96
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-26 08:41:35 +0700

    Update delsstp.sh

--- sstp/delsstp.sh
+++ sstp/delsstp.sh
@@ -16,22 +16,33 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/wisnucs/data-user-sstp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\E[44;1;39m     ⇱ Delete SSTP Account ⇲       \E[0m"
+        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 		echo ""
 		echo "You have no existing clients!"
-		exit 1
+		echo ""
+		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+		read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu"
+        l2tppmenu
 	fi
 
-	echo ""
-	echo " Select the existing client you want to remove"
-	echo " Press CTRL+C to return"
-	echo " ==============================="
-	echo "     No  Expired   User"
+	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[44;1;39m     ⇱ Delete SSTP Account ⇲       \E[0m"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+	echo "     No  User.   Expired "
+	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	grep -E "^### " "/var/lib/wisnucs/data-user-sstp" | cut -d ' ' -f 2-3 | nl -s ') '
+	echo -e "     0) Cancel"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Pilih salah satu[1]: " CLIENT_NUMBER
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+	    elif [[ ${CLIENT_NUMBER} == '0' ]]; then
+            sstp-menu
 		else
-			read -rp "Pilih salah satu [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
 Client=$(grep -E "^###" /var/lib/wisnucs/data-user-sstp | cut -d ' ' -f 2-3 | sed -n "$CLIENT_NUMBER"p)
@@ -40,11 +51,12 @@ exp=$(grep -E "^###" /var/lib/wisnucs/data-user-sstp | cut -d ' ' -f 3 | sed -n 
 sed -i "/^### $user $exp/d" /var/lib/wisnucs/data-user-sstp
 sed -i '/^'"$user"'/d' /home/sstp/sstp_account
 clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo " SSTP Account Has Been Successfully Deleted"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo " Client Name : $user"
+echo " Expired On  : $exp"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-echo "=========================="
-echo "   SSTP Account Deleted   "
-echo "=========================="
-echo "Username  : $user"
-echo "Expired   : $exp"
-echo "=========================="
-echo "Script By @zerossl"
+read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
+menu
