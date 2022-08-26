5ef0c722379c6fdc708c9ddb995975e0597a6a59
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-26 08:24:47 +0700

    Update addtrojanquic.sh

--- xray/addtrojanquic.sh
+++ xray/addtrojanquic.sh
@@ -65,3 +65,6 @@ echo -e "TROJAN QUIC: ${trojanquic}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m🔰LUXURY EDITION ZEROSSL🔰\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
+menu
