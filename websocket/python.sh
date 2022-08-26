028db4bb98fc1e07f186792382455366377916fe
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-27 05:34:46 +0700

    Update python.sh

--- websocket/python.sh
+++ websocket/python.sh
@@ -50,7 +50,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/nontls 2052
+ExecStart=/usr/bin/python -O /usr/local/bin/nontls 8880
 Restart=on-failure
 
 [Install]
