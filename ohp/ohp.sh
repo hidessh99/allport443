9dd6432af101064ebbcb4a461fa3432f9655a3d0
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-27 05:42:31 +0700

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -36,7 +36,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/ohpserver -port 8181 -proxy 127.0.0.1:3228 -tunnel 127.0.0.1:22
+ExecStart=/usr/local/bin/ohpserver -port 8181 -proxy 127.0.0.1:3228 -tunnel 127.0.0.1:200
 Restart=on-failure
 LimitNOFILE=infinity
 
@@ -99,7 +99,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/ohpserver -port 8484 -proxy 127.0.0.1:3228 -tunnel 127.0.0.1:700
+ExecStart=/usr/local/bin/ohpserver -port 8484 -proxy 127.0.0.1:3228 -tunnel 127.0.0.1:8443
 Restart=on-failure
 LimitNOFILE=infinity
 
