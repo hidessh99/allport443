f207f950d7a1ec32f4ce22666cbe21de23955a00
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-26 10:35:28 +0700

    Update addvmessquic.sh

--- xray/addvmessquic.sh
+++ xray/addvmessquic.sh
@@ -134,3 +134,6 @@ echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━�
 #echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m🔰LUXURY EDITION ZEROSSL🔰\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
+menu
