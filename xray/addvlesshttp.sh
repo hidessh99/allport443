d3dd94d5f39d7a151846c5318ac4d5346d9b23f3
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-26 09:56:28 +0700

    Update addvlesshttp.sh

--- xray/addvlesshttp.sh
+++ xray/addvlesshttp.sh
@@ -120,4 +120,6 @@ echo -e "VLESS HTTP NONTLS: ${vlesshttpnon}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\033[1;46m🔰LUXURY EDITION ZEROSSL🔰\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
+echo ""
+read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
+menu
