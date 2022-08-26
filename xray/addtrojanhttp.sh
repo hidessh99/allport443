8397e5a836d4207ca4f6a1e0a90f2ae96b05eff6
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-27 05:24:00 +0700

    Update addtrojanhttp.sh

--- xray/addtrojanhttp.sh
+++ xray/addtrojanhttp.sh
@@ -79,7 +79,7 @@ systemctl restart xray.service
 systemctl restart xtrojan.service
 systemctl restart xvmess.service
 systemctl restart xvless
-systemctl restart nginx
+#systemctl restart nginx
 systemctlrestart xss
 service cron restart
 
