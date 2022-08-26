b6f54b9b6c3ee8da5c19a46b745a6db376daae49
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-27 05:27:03 +0700

    Update addvlessgrpc.sh

--- xray/addvlessgrpc.sh
+++ xray/addvlessgrpc.sh
@@ -78,7 +78,7 @@ systemctl restart xvless.service
 systemctl restart xray.service
 systemctl restart xvmess
 systemctl restart xtrojan
-systemctl restart nginx
+#systemctl restart nginx
 service cron restart
 clear
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
