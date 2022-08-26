b7b1e8eb977cc738279734b00b10e7cf72342007
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-27 05:25:12 +0700

    Update addtrojantester.sh

--- xray/addtrojantester.sh
+++ xray/addtrojantester.sh
@@ -124,7 +124,7 @@ systemctl restart xray.service
 systemctl restart xtrojan.service
 systemctl restart xvless
 systemctl restart xvmess
-systemctl restart nginx
+#systemctl restart nginx
 service cron restart
 clear
 echo -e ""
