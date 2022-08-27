e00e95fbc6a0d4e5ec4546e2f39676768fb3db6b
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-28 06:09:17 +0700

    Update vpn.sh

--- ssh/vpn.sh
+++ ssh/vpn.sh
@@ -52,7 +52,7 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
 # Buat config client TCP 600
-cat > /etc/openvpn/tcp.ovpn <<-END
+cat > /etc/openvpn/OVPN-TCP.ovpn <<-END
 client
 dev tun
 proto tcp
@@ -70,7 +70,7 @@ END
 sed -i $MYIP2 /etc/openvpn/tcp.ovpn;
 
 # Buat config client UDP 800
-cat > /etc/openvpn/udp.ovpn <<-END
+cat > /etc/openvpn/OVPN-UDP.ovpn <<-END
 client
 dev tun
 proto udp
@@ -85,28 +85,10 @@ comp-lzo
 verb 3
 END
 
-sed -i $MYIP2 /etc/openvpn/udp.ovpn;
+sed -i $MYIP2 /etc/openvpn/OVPN-UDP.ovpn;
 
 # Buat config client SSL
-cat > /etc/openvpn/ssl.ovpn <<-END
-client
-dev tun
-proto tcp
-remote xxxxxxxxx 900
-resolv-retry infinite
-route-method exe
-nobind
-persist-key
-persist-tun
-auth-user-pass
-comp-lzo
-verb 3
-END
-
-sed -i $MYIP2 /etc/openvpn/ssl.ovpn;
-
-# Buat config client ws
-cat > /etc/openvpn/ws.ovpn <<-END
+cat > /etc/openvpn/OVPN-SSL-WS.ovpn <<-END
 client
 dev tun
 proto tcp
@@ -121,32 +103,32 @@ comp-lzo
 verb 3
 END
 
-sed -i $MYIP2 /etc/openvpn/ws.ovpn;
+sed -i $MYIP2 /etc/openvpn/OVPN-SSL-WS.ovpn;
 
 cd
 # pada tulisan xxx ganti dengan alamat ip address VPS anda 
 /etc/init.d/openvpn restart
 
 # masukkan certificatenya ke dalam config client TCP 600
-echo '<ca>' >> /etc/openvpn/tcp.ovpn
-cat /etc/openvpn/server/ca.crt >> /etc/openvpn/tcp.ovpn
-echo '</ca>' >> /etc/openvpn/tcp.ovpn
+echo '<ca>' >> /etc/openvpn/GANDRING-TCP.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/GANDRING-TCP.ovpn
+echo '</ca>' >> /etc/openvpn/GANDRING-TCP.ovpn
 
-# Copy config OpenVPN client ke home directory root agar mudah didownload ( TCP 700 )
-cp /etc/openvpn/tcp.ovpn /home/vps/public_html/tcp.ovpn
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( TCP 600 )
+cp /etc/openvpn/GANDRING-TCP.ovpn /home/vps/public_html/GANDRING-TCP.ovpn
 
 # masukkan certificatenya ke dalam config client UDP 800
-echo '<ca>' >> /etc/openvpn/udp.ovpn
-cat /etc/openvpn/server/ca.crt >> /etc/openvpn/udp.ovpn
-echo '</ca>' >> /etc/openvpn/udp.ovpn
+echo '<ca>' >> /etc/openvpn/GANDRING-UDP.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/GANDRING-UDP.ovpn
+echo '</ca>' >> /etc/openvpn/GANDRING-UDP.ovpn
 
 # Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 800 )
-cp /etc/openvpn/udp.ovpn /home/vps/public_html/udp.ovpn
+cp /etc/openvpn/GANDRING-UDP.ovpn /home/vps/public_html/GANDRING-UDP.ovpn
 
-# masukkan certificatenya ke dalam config client SSL 900
-echo '<ca>' >> /etc/openvpn/ssl.ovpn
-cat /etc/openvpn/server/ca.crt >> /etc/openvpn/ssl.ovpn
-echo '</ca>' >> /etc/openvpn/ssl.ovpn
+# masukkan certificatenya ke dalam config client SSL 443
+echo '<ca>' >> /etc/openvpn/GANDRING-SSL-WS.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/GANDRING-SSL-WS.ovpn
+echo '</ca>' >> /etc/openvpn/GANDRING-SSL-WS.ovpn
 
 #echo '<cert>' >> /etc/openvpn/ssl.ovpn
 #cat /etc/openvpn/server/server.crt >> /etc/openvpn/ssl.ovpn
@@ -161,34 +143,14 @@ echo '</ca>' >> /etc/openvpn/ssl.ovpn
 #echo '</tls-auth>' >> /etc/openvpn/ssl.ovpn
 
 # Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 800 )
-cp /etc/openvpn/ssl.ovpn /home/vps/public_html/ssl.ovpn
-
-# masukkan certificatenya ke dalam config client SSL 900
-echo '<ca>' >> /etc/openvpn/ws.ovpn
-cat /etc/openvpn/server/ca.crt >> /etc/openvpn/ws.ovpn
-echo '</ca>' >> /etc/openvpn/ws.ovpn
-
-#echo '<cert>' >> /etc/openvpn/ssl.ovpn
-#cat /etc/openvpn/server/server.crt >> /etc/openvpn/ssl.ovpn
-#echo '</cert>' >> /etc/openvpn/ssl.ovpn
-
-#echo '<key>' >> /etc/openvpn/ws.ovpn
-#cat /etc/xray/xray.key >> /etc/openvpn/ws.ovpn
-#echo '</key>' >> /etc/openvpn/ws.ovpn
-
-#echo '<tls-auth>' >> /etc/openvpn/ssl.ovpn
-#cat /etc/openvpn/server/ta.key >> /etc/openvpn/ssl.ovpn
-#echo '</tls-auth>' >> /etc/openvpn/ssl.ovpn
-
-# Copy config OpenVPN client ke home directory root agar mudah didownload ( SSL 900 )
-cp /etc/openvpn/ws.ovpn /home/vps/public_html/ws.ovpn
+cp /etc/openvpn/GANDRING-SSL-WS.ovpn /home/vps/public_html/GANDRING-SSL-WS.ovpn
 
 #firewall untuk memperbolehkan akses UDP dan akses jalur TCP
 
 iptables -t nat -I POSTROUTING -s 10.1.0.0/32 -o $ANU -j MASQUERADE
 iptables -t nat -I POSTROUTING -s 10.2.0.0/32 -o $ANU -j MASQUERADE
-iptables -t nat -I POSTROUTING -s 10.3.0.0/32 -o $ANU -j MASQUERADE
-iptables -t nat -I POSTROUTING -s 10.4.0.0/32 -o $ANU -j MASQUERADE
+#iptables -t nat -I POSTROUTING -s 10.3.0.0/32 -o $ANU -j MASQUERADE
+#iptables -t nat -I POSTROUTING -s 10.4.0.0/32 -o $ANU -j MASQUERADE
 iptables-save > /etc/iptables.up.rules
 chmod +x /etc/iptables.up.rules
 
@@ -202,7 +164,7 @@ systemctl start openvpn
 /etc/init.d/openvpn restart
 
 cd /home/vps/public_html/
-zip gandring.zip tcp.ovpn udp.ovpn ssl.ovpn ws.ovpn > /dev/null 2>&1
+zip GANDRING.zip GANDRING-TCP.ovpn GANDRING-UDP.ovpn GANDRING-SSL-WS.ovpn > /dev/null 2>&1
 cd
 cat <<'mySiteOvpn' > /home/vps/public_html/index.html
 <!DOCTYPE html>
@@ -212,15 +174,13 @@ cat <<'mySiteOvpn' > /home/vps/public_html/index.html
 
 <head><meta charset="utf-8" /><title>BENDUNG COLO PENGKOL</title><meta name="description" content="Server" /><meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" /><meta name="theme-color" content="#000000" /><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"><link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"><link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.3/css/mdb.min.css" rel="stylesheet"></head><body><div class="container justify-content-center" style="margin-top:9em;margin-bottom:5em;"><div class="col-md"><div class="view"><img src="https://openvpn.net/wp-content/uploads/openvpn.jpg" class="card-img-top"><div class="mask rgba-white-slight"></div></div><div class="card"><div class="card-body"><h5 class="card-title">Config List</h5><br /><ul class="list-group">
 
-<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>TCP <span class="badge light-blue darken-4">KONFIG PENAKLUK JANDA</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/tcp.ovpn" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
+<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>TCP <span class="badge light-blue darken-4">KONFIG PENAKLUK JANDA</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/GANDRING-TCP.ovpn" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
 
-<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>UDP <span class="badge light-blue darken-4">KONFIG PENAKLUK PERAWAN</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/udp.ovpn" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
+<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>UDP <span class="badge light-blue darken-4">KONFIG PENAKLUK PERAWAN</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/GANDRING-UDP.ovpn" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
 
-<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>SSL <span class="badge light-blue darken-4">KONFIG RUSAK</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/ssl.ovpn" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
+<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>WS/SSL <span class="badge light-blue darken-4">KONFIG WS/SSL</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/GANDRING-SSL-WS.ovpn" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
 
-<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>WS <span class="badge light-blue darken-4">KONFIG REMOK</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/ws.ovpn" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
-
-<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>GANDRING.zip <span class="badge light-blue darken-4">HATI YANG GEMBIRA ADALAH OBAT AWET MUDA</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/gandring.zip" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
+<li class="list-group-item justify-content-between align-items-center" style="margin-bottom:1em;"><p>GANDRING.zip <span class="badge light-blue darken-4">HATI YANG GEMBIRA ADALAH OBAT AWET MUDA</span><br /><small></small></p><a class="btn btn-outline-success waves-effect btn-sm" href="http://IP-ADDRESSS:88/GANDRING.zip" style="float:right;"><i class="fa fa-download"></i> Download</a></li>
 
 </ul></div></div></div></div></body></html>
 mySiteOvpn
