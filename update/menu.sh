0cf9e7916f01332724d78e36d8a76d9b0c09b962
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-08-30 03:02:31 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -22,10 +22,10 @@ echo -e "08\e[0;33m) MENU LAYANAN KONFIGURASI SERVER ANDA \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "00\e[1;31m) NOMER IKI METU OM \e[m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m             🔰 LUXURY EDITION BY ZEROSSL 🔰            \E[0m"
+echo -e "\033[1;46m              🔰 LUXURY EDITION ZEROSSL 🔰              \E[0m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[0;31m   ⇱ TEKAN K UNTUK KE VERSI LONDO ⇲  \e[m"
-echo -e "\e[0;31m   ⇱ PRESS K FOR ENGLISH VERSION ⇲   \e[m"
+echo -e "\e[0;31m   🌏 TEKAN K UNTUK KE VERSI LONDO   \e[m"
+echo -e "\e[0;31m   🌏 PRESS K FOR ENGLISH VERSION    \e[m"
 read -p "Silahkan Pilih Angka [ 1 - 0 ] ⇲: " menu
 echo -e ""
 case $menu in
