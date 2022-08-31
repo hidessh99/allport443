e70ca2e7cc6603801ad2db2bc85b17fa85d063dd
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-01 04:03:35 +0700

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -14,7 +14,71 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
+# Direct Proxy Squid For OpenVPN TCP
 
+RED='\e[1;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+NC='\e[0m'
+MYIP=$(wget -qO- https://icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# Update Repository VPS
+clear
+apt update 
+apt-get -y upgrade
+
+# Port Server
+Port_OpenVPN_TCP='700';
+Port_Squid='3128';
+Port_Openvpn='8383';
+Port_Dropbear='8282';
+Port_ssh='8181';
+
+# Installing ohp Server
+cd 
+wget -O /usr/local/bin/ohp "https://raw.githubusercontent.com/geovpn/bc/main/ohp/ohp"
+chmod +x /usr/local/bin/ohp
+# Buat File OpenVPN TCP OHP
+cat > /etc/openvpn/GANDRING-SSL-WS.ovpn <<END
+setenv CLIENT_CERT 0
+setenv opt block-outside-dns
+client
+dev tun
+proto tcp
+remote xxxxxxxxx 443
+persist-tun
+persist-key
+persist-remote-ip
+comp-lzo
+reneg-sec 0
+pull
+resolv-retry infinite
+remote-cert-tls server
+verb 3
+auth-user-pass
+cipher none
+auth none
+auth-nocache
+script-security 2
+tls-version-min 1.2
+tls-cipher TLS-ECDHE-ECDSA-WITH-AES-128-GCM-SHA256
+http-proxy xxxxxxxxx 8080
+http-proxy-option VERSION 1.1
+http-proxy-option CUSTOM-HEADER ""
+http-proxy-option CUSTOM-HEADER "Host: "
+http-proxy-option CUSTOM-HEADER "X-Forwarded-Host: "
+http-proxy-option CUSTOM-HEADER ""
+END
+sed -i $MYIP2 /etc/openvpn/GANDRING-SSL-WS.ovpn;
+
+# masukkan certificatenya ke dalam config client TCP 443
+echo '<ca>' >> /etc/openvpn/GANDRING-SSL-WS.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/GANDRING-SSL-WS.ovpn
+echo '</ca>' >> /etc/openvpn/GANDRING-SSL-WS.ovpn
+cp /etc/openvpn/GANDRING-SSL-WS.ovpn /home/vps/public_html/GANDRING-SSL-WS.ovpn
+clear
+cd 
 # Download File Ohp
 wget https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
 unzip ohpserver-linux32.zip
@@ -65,7 +129,7 @@ LimitNOFILE=infinity
 WantedBy=multi-user.target
 END
 
-# OpenVPN OHP 8383
+# OpenVPN OHP 8080
 cat > /etc/systemd/system/openvpn-ohp.service << END
 [Unit]]
 Description=OpenVPN OHP Redirection Service
@@ -78,7 +142,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/ohpserver -port 8383 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:700
+ExecStart=/usr/local/bin/ohpserver -port 8080 -proxy 127.0.0.1:3128 -tunnel 127.0.0.1:700
 Restart=on-failure
 LimitNOFILE=infinity
 
@@ -111,7 +175,7 @@ else
 	echo 'Dropbear OHP Redirection Not Found, please check manually'
 fi
 sleep 0.5
-if [ -n "$(ss -tupln | grep ohpserver | grep -w 8384)" ]
+if [ -n "$(ss -tupln | grep ohpserver | grep -w 8080)" ]
 then
 	echo 'OpenVPN OHP Redirection Running'
 else
