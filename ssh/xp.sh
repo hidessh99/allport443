7ab8914575427e4437acb0956f4756d712a8a1c2
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-03 04:29:11 +0700

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -14,6 +14,36 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
+##------ Auto Remove SSH
+hariini=`date +%d-%m-%Y`
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+for((i=1; i<=$totalaccounts; i++ ))
+do
+tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+username=`echo $tuserval | cut -f1 -d:`
+userexp=`echo $tuserval | cut -f2 -d:`
+userexpireinseconds=$(( $userexp * 86400 ))
+tglexp=`date -d @$userexpireinseconds`             
+tgl=`echo $tglexp |awk -F" " '{print $3}'`
+while [ ${#tgl} -lt 2 ]
+do
+tgl="0"$tgl
+done
+while [ ${#username} -lt 15 ]
+do
+username=$username" " 
+done
+bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+todaystime=`date +%s`
+if [ $userexpireinseconds -ge $todaystime ] ;
+then
+:
+else
+userdel --force $username
+fi
+done
+
 data=( `cat /var/lib/wisnucs/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
