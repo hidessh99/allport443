41085eadedc7f8908ebab404f19d11b803f2e0bb
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-05 06:39:20 +0700

    Update ipsec.sh

--- ipsec/ipsec.sh
+++ ipsec/ipsec.sh
@@ -50,23 +50,23 @@ REPO3='--enablerepo=*releases-optional*'
 REPO4='--enablerepo=PowerTools'
 
 yum -y install nss-devel nspr-devel pkgconfig pam-devel \
-libcap-ng-devel libselinux-devel curl-devel nss-tools \
-flex bison gcc make ppp 
+  libcap-ng-devel libselinux-devel curl-devel nss-tools \
+  flex bison gcc make ppp 
 
 yum "$REPO1" -y install xl2tpd 
 
 if [[ $ver == '7' ]]; then
-yum -y install systemd-devel iptables-services 
-yum "$REPO2" "$REPO3" -y install libevent-devel fipscheck-devel 
+  yum -y install systemd-devel iptables-services 
+  yum "$REPO2" "$REPO3" -y install libevent-devel fipscheck-devel 
 elif [[ $ver == '8' ]]; then
-yum "$REPO4" -y install systemd-devel libevent-devel fipscheck-devel 
+  yum "$REPO4" -y install systemd-devel libevent-devel fipscheck-devel 
 fi
 else
 apt install openssl iptables iptables-persistent -y
 apt-get -y install libnss3-dev libnspr4-dev pkg-config \
-libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev \
-libcurl4-nss-dev flex bison gcc make libnss3-tools \
-libevent-dev ppp xl2tpd pptpd
+  libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev \
+  libcurl4-nss-dev flex bison gcc make libnss3-tools \
+  libevent-dev ppp xl2tpd pptpd
 fi
 bigecho "Compiling and installing Libreswan..."
 
@@ -75,7 +75,7 @@ swan_file="libreswan-$SWAN_VER.tar.gz"
 swan_url1="https://github.com/libreswan/libreswan/archive/v$SWAN_VER.tar.gz"
 swan_url2="https://download.libreswan.org/$swan_file"
 if ! { wget -t 3 -T 30 -nv -O "$swan_file" "$swan_url1" || wget -t 3 -T 30 -nv -O "$swan_file" "$swan_url2"; }; then
-exit 1
+  exit 1
 fi
 /bin/rm -rf "/opt/src/libreswan-$SWAN_VER"
 tar xzf "$swan_file" && /bin/rm -f "$swan_file"
@@ -90,15 +90,16 @@ USE_NSS_IPSEC_PROFILE = false
 USE_GLIBC_KERN_FLIP_HEADERS = true
 EOF
 if ! grep -qs IFLA_XFRM_LINK /usr/include/linux/if_link.h; then
-echo "USE_XFRM_INTERFACE_IFLA_HEADER = true" >> Makefile.inc.local
+  echo "USE_XFRM_INTERFACE_IFLA_HEADER = true" >> Makefile.inc.local
 fi
 if [[ ${OS} == "debian" ]]; then
 if [ "$(packaging/utils/lswan_detect.sh init)" = "systemd" ]; then
-apt-get -y install libsystemd-dev
-fi
+  apt-get -y install libsystemd-dev
+  fi
 elif [[ ${OS} == "ubuntu" ]]; then
 if [ "$(packaging/utils/lswan_detect.sh init)" = "systemd" ]; then
-apt-get -y install libsystemd-dev
+  apt-get -y install libsystemd-dev
+fi
 fi
 NPROCS=$(grep -c ^processor /proc/cpuinfo)
 [ -z "$NPROCS" ] && NPROCS=1
@@ -107,7 +108,7 @@ make "-j$((NPROCS+1))" -s base && make -s install-base
 cd /opt/src || exit 1
 /bin/rm -rf "/opt/src/libreswan-$SWAN_VER"
 if ! /usr/local/sbin/ipsec --version 2>/dev/null | grep -qF "$SWAN_VER"; then
-exiterr "Libreswan $SWAN_VER failed to build."
+  exiterr "Libreswan $SWAN_VER failed to build."
 fi
 
 bigecho "Creating VPN configuration..."
@@ -127,57 +128,58 @@ cat > /etc/ipsec.conf <<EOF
 version 2.0
 
 config setup
-virtual-private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!$L2TP_NET,%v4:!$XAUTH_NET
-protostack=netkey
-interfaces=%defaultroute
-uniqueids=no
+  virtual-private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!$L2TP_NET,%v4:!$XAUTH_NET
+  protostack=netkey
+  interfaces=%defaultroute
+  uniqueids=no
 
 conn shared
-left=%defaultroute
-leftid=$PUBLIC_IP
-right=%any
-encapsulation=yes
-authby=secret
-pfs=no
-rekey=no
-keyingtries=5
-dpddelay=30
-dpdtimeout=120
-dpdaction=clear
-ikev2=never
-ike=aes256-sha2,aes128-sha2,aes256-sha1,aes128-sha1,aes256-sha2;modp1024,aes128-sha1;modp1024
-phase2alg=aes_gcm-null,aes128-sha1,aes256-sha1,aes256-sha2_512,aes128-sha2,aes256-sha2
-sha2-truncbug=no
+  left=%defaultroute
+  leftid=$PUBLIC_IP
+  right=%any
+  encapsulation=yes
+  authby=secret
+  pfs=no
+  rekey=no
+  keyingtries=5
+  dpddelay=30
+  dpdtimeout=120
+  dpdaction=clear
+  ikev2=never
+  ike=aes256-sha2,aes128-sha2,aes256-sha1,aes128-sha1,aes256-sha2;modp1024,aes128-sha1;modp1024
+  phase2alg=aes_gcm-null,aes128-sha1,aes256-sha1,aes256-sha2_512,aes128-sha2,aes256-sha2
+  sha2-truncbug=no
 
 conn l2tp-psk
-auto=add 
-leftprotoport=17/1701
-rightprotoport=17/%any
-type=transport
-phase2=esp
-also=shared
+  auto=add
+  leftprotoport=17/1701
+  rightprotoport=17/%any
+  type=transport
+  phase2=esp
+  also=shared
 
 conn xauth-psk
-auto=add
-leftsubnet=0.0.0.0/0
-rightaddresspool=$XAUTH_POOL
-modecfgdns=$DNS_SRVS
-leftxauthserver=yes
-rightxauthclient=yes
-leftmodecfgserver=yes
-rightmodecfgclient=yes
-modecfgpull=yes
-xauthby=file
-ike-frag=yes
-cisco-unity=yes
-also=shared
+  auto=add
+  leftsubnet=0.0.0.0/0
+  rightaddresspool=$XAUTH_POOL
+  modecfgdns=$DNS_SRVS
+  leftxauthserver=yes
+  rightxauthclient=yes
+  leftmodecfgserver=yes
+  rightmodecfgclient=yes
+  modecfgpull=yes
+  xauthby=file
+  ike-frag=yes
+  cisco-unity=yes
+  also=shared
 
 include /etc/ipsec.d/*.conf
 EOF
 
 if uname -m | grep -qi '^arm'; then
-if ! modprobe -q sha512; then
-sed -i '/phase2alg/s/,aes256-sha2_512//' /etc/ipsec.conf
+  if ! modprobe -q sha512; then
+    sed -i '/phase2alg/s/,aes256-sha2_512//' /etc/ipsec.conf
+  fi
 fi
 
 # Specify IPsec PSK
@@ -278,8 +280,8 @@ systemctl enable ipsec
 systemctl enable pptpd
 
 for svc in fail2ban ipsec xl2tpd; do
-update-rc.d "$svc" enable >/dev/null 2>&1
-systemctl enable "$svc" 2>/dev/null
+  update-rc.d "$svc" enable >/dev/null 2>&1
+  systemctl enable "$svc" 2>/dev/null
 done
 
 bigecho "Starting services..."
@@ -296,6 +298,6 @@ wget -O /usr/bin/addpptp https://${wisnuvpn}/addpptp.sh && chmod +x /usr/bin/add
 wget -O /usr/bin/delpptp https://${wisnuvpn}/delpptp.sh && chmod +x /usr/bin/delpptp
 wget -O /usr/bin/renewpptp https://${wisnuvpn}/renewpptp.sh && chmod +x /usr/bin/renewpptp
 wget -O /usr/bin/renewl2tp https://${wisnuvpn}/renewl2tp.sh && chmod +x /usr/bin/renewl2tp
-touch /var/lib/wisnucs/data-user-l2tp
-touch /var/lib/wisnucs/data-user-pptp
+touch /var/lib/crot/data-user-l2tp
+touch /var/lib/crot/data-user-pptp
 rm -f /root/ipsec.sh
