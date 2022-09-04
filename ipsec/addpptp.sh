3bf6f0be9b425394e0ad5ecffa2ecf056a660100
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-04 08:55:43 +0700

    Update addpptp.sh

--- ipsec/addpptp.sh
+++ ipsec/addpptp.sh
@@ -1,43 +1,38 @@
 #!/bin/bash
-# My Telegram : https://t.me/zerossl
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+#Remake by zerossl
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+domain=$(cat /etc/xray/domain)
 clear
+source /var/lib/wisnucs/ipvps.conf
 if [[ "$IP" = "" ]]; then
-PUBLIC_IP=$(wget -qO- ipinfo.io/ip);
+PUBLIC_IP=$(curl -sS ifconfig.me);
 else
 PUBLIC_IP=$IP
 fi
-source /var/lib/wisnucs/ipvps.conf
-if [[ "$IP2" = "" ]]; then
-domain=$(cat /etc/xray/domain)
-#domain=$(cat /etc/v2ray/domain)
-else
-domain=$IP2
-fi
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  | tee -a /etc/log-create-user.log
+echo -e "\E[46;1;46m        🔰 AKUN PPTP 🔰           \E[0m"   | tee -a /etc/log-create-user.log
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log		
+
 until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "Username : " -e VPN_USER
+		read -rp "Username: " -e VPN_USER
 		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/wisnucs/data-user-pptp | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  | tee -a /etc/log-create-user.log
+echo -e "\E[46;1;46m        🔰 AKUN PPTP 🔰           \E[0m"   | tee -a /etc/log-create-user.log
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log		
 			echo ""
-			echo -e "Username ${RED}${VPN_USER}${NC} Already On VPS Please Choose Another"
-			exit 1
+			echo "A client with the specified name was already created, please choose another name."
+			echo ""
+            read -n 1 -s -r -p "Press any key to back on menu"
+            
+            l2tppmenu
 		fi
 	done
-read -p "Password : " VPN_PASSWORD
-read -p "Expired (Days) : " masaaktif
+read -p "Password: " VPN_PASSWORD
+read -p "Expired (days): " masaaktif
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 clear
@@ -50,19 +45,22 @@ EOF
 # Update file attributes
 chmod 600 /etc/ppp/chap-secrets*
 echo -e "### $VPN_USER $exp">>"/var/lib/wisnucs/data-user-pptp"
-cat <<EOF
-echo -e ""
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[46;1;46m        🔰 AKUN PPTP 🔰           \E[0m"   
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "✨ IP ➡️ $PUBLIC_IP"
-echo -e "✨ Domain ➡️ $domain"
-echo -e "✨ Username➡️ $VPN_USER"
-echo -e "✨ Password ➡️ $VPN_PASSWORD"
-echo -e "✨ Dibuat ➡️ $hariini"
-echo -e "✨ Kadaluarsa ➡️ $exp"
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[46;1;46m  🔰LUXURY EDITION BY ZEROSSL🔰   \E[0m"   
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-EOF
+clear
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  | tee -a /etc/log-create-user.log
+echo -e "\E[46;1;46m        🔰 AKUN PPTP 🔰           \E[0m"   | tee -a /etc/log-create-user.log
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
+echo -e "✨IP ➡️ $PUBLIC_IP" | tee -a /etc/log-create-user.log
+echo -e "✨DOMAIN ➡️ $domain"
+echo -e "✨Username ➡️ $VPN_USER" | tee -a /etc/log-create-user.log
+echo -e "✨Password ➡️ $VPN_PASSWORD" | tee -a /etc/log-create-user.log
+echo -e "✨Dibuat ➡️ $hariini"
+echo -e "✨Kadaluarsa ➡️ $exp" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[46;1;46m    🔰LUXURY EDITION ZEROSSL🔰    \E[0m"  | tee -a /etc/log-create-user.log
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
+read -n 1 -s -r -p "Tekan Bebas Untuk Ke Menu"
+
+l2tppmenu
