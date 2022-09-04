d1475b60f9e699b1b332d5363a552616dec45243
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-04 20:05:47 +0700

    Update ssr.sh

--- ssr/ssr.sh
+++ ssr/ssr.sh
@@ -45,8 +45,8 @@ check_pid(){
 	PID=`ps -ef |grep -v grep | grep server.py |awk '{print $2}'`
 }
 Add_iptables(){
-		iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
-		iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
+		iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1444-1944 -j ACCEPT
+		iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1444-1944 -j ACCEPT
 }
 Save_iptables(){
 if [[ ${OS} == "centos" ]]; then
