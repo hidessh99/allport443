703ea3891a0976797761529d780b22e612a021cb
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-04 09:01:50 +0700

    Update renewl2tp.sh

--- ipsec/renewl2tp.sh
+++ ipsec/renewl2tp.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
-# My Telegram : https://t.me/zerossl
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
 clear
+
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/wisnucs/data-user-l2tp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
+        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━\033[0m"		
+        echo -e "\E[44;1;39m        ✨ Renew L2TP User ✨        \E[0m"
+        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━\033[0m"		
 		echo ""
 		echo "You have no existing clients!"
-		exit 1
+		echo ""
+		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"		
+        read -n 1 -s -r -p "Press any key to back on menu"
+        ipsec-menu
 	fi
 
 	clear
-	echo ""
-	echo "Select the existing client you want to renew"
-	echo " Press CTRL+C to return"
-	echo -e "==============================="
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━\033[0m"		
+    echo -e "\E[44;1;39m        ✨ Renew L2TP User ✨        \E[0m"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━\033[0m"		
 	grep -E "^### " "/var/lib/wisnucs/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
+	echo -e "     0) Cancel"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
+	    elif [[ ${CLIENT_NUMBER} == '0' ]]; then
+            l2tppmenu
 		else
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-read -p "Expired (Days) : " masaaktif
-user=$(grep -E "^### " "/var/lib/wisnucs/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/var/lib/wisnucs/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+read -p "Expired (days): " masaaktif
+user=$(grep -E "^### " "/var/lib/wisnucs/data-user-l2tp" | cut -d ' ' -f2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/var/lib/wisnucs/data-user-l2tp" | cut -d ' ' -f3 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
@@ -46,11 +46,13 @@ exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
 sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/wisnucs/data-user-l2tp
 clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"		
+echo " AKUN L2TP YANG DIPERPANJANG"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"		
+echo "✨Nama User ➡️ $user"
+echo "✨Kadaluarsa ➡️ $exp4"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━\033[0m"		
 echo ""
-echo "=========================="
-echo "   L2TP Account Renewed   "
-echo "=========================="
-echo "Username  : $user"
-echo "Expired   : $exp4"
-echo "=========================="
-echo "Script By @zerossl"
+read -n 1 -s -r -p "Tekan Bebas Untuk Ke Menu"
+
+l2tppmenu
