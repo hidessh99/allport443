830b5f80b9892883c3b0bc2b362a1697fab31990
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-05 21:24:44 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -732,7 +732,7 @@ cat > /etc/trojan-go/config.json << END
 {
   "run_type": "server",
    "local_addr": "0.0.0.0",
-    "local_port": 2087,
+    "local_port": 8443,
      "remote_addr": "127.0.0.1",
       "remote_port": 88,
        "log_level": 1,
