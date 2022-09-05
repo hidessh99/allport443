6a7a937f43f1d515ed8227ce2eafcfc972a2b06a
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-05 08:06:20 +0700

    Update sstp.sh

--- sstp/sstp.sh
+++ sstp/sstp.sh
@@ -73,12 +73,12 @@ systemctl enable accel-ppp
 #gen cert sstp
 cd /home/sstp
 openssl genrsa -out ca.key 2048
-openssl req -new -x509 -days 3650 -key ca.key -out ca.crt \
+openssl req -new -x509 -days 36500 -key ca.key -out ca.crt \
 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out ia.csr \
 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
-openssl x509 -req -days 3650 -in ia.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+openssl x509 -req -days 36500 -in ia.csr -TAkey ta.key -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 cp /home/sstp/server.crt /home/vps/public_html/server.crt
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 444 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 444 -j ACCEPT
